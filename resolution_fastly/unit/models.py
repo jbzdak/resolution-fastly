39e44cea96bcab554b1d609103b3a038660e5af1
Jacek Bzdak <jbzdak@gmail.com>
2017-04-09 23:42:17 +0200

    WIP

--- resolution_fastly/unit/models.py
+++ resolution_fastly/unit/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Voter(models.Model):
+
+    slack_id = models.CharField(max_length=512, null=True, blank=True)
+    full_name = models.CharField(max_length=1024, null=False, blank=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+
+
+class OrganisationalUnit(models.Model):
+
+    name = models.CharField(max_length=512)
+
+    owners = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    voters = models.ManyToManyField(Voter)
+
+
+
+
+
